48b4c0ab41e8c3a92a8920fd6a69419423976195
syspek <loki99907@gmail.com>
2019-10-12 03:07:00 +0300

    сдача второго ДЗ

---
+++ lesson_2.sql
@@ -0,0 +1,18 @@
+[andrey_sadovoy]
+--задания ко 2 уроку
+CREATE DATABASE IF NOT EXISTS example;
+USE example;
+CREATE TABLE IF EXISTS user(id INT (128), name VARCHAR (128));
+
+CREATE DATABASE IF NOT EXISTS simple;
+
+/*
+
+команды в терминале:
+mysqldump example > example.sql - дамп example
+mysql simple < example.sql - развертывание дампа
+
+
+mysqldump --where="1 limit 100" mysql help_keyword > table_help_keyword.sql - дамп по параметрам (изтаблицы
+help_keyword создаю дамп с ограничением 100 строк
+*/
